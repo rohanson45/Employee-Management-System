08fdcf979d237851d647a687d054236bfbf62d5a
rohanson45 <rohanson45@gmail.com>
2026-06-28 17:17:42 +0530

    Fix SQLite path for Azure deployment with auto-migration

--- Program.cs
+++ Program.cs
@@ -2,14 +2,22 @@ using Microsoft.EntityFrameworkCore;
 using EmployeeManagementSystem.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var dbPath = builder.Environment.IsDevelopment()
+    ? "employees.db"
+    : "/home/employees.db";
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite($"Data Source={dbPath}"));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
